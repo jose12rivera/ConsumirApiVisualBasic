dabddfa0078edbbd8dbd5f722ee385cdaf3849ac
José Daniel Rivera Ortega <jose_rivera1@ucne.edu.do>
2025-03-08 22:13:01 -0400

    La clase provedor

---
+++ Models/Proveedor.vb
@@ -0,0 +1,6 @@
+﻿Public Class Proveedor
+    Public Property proovedorId As Integer
+    Public Property nombre As String
+    Public Property contacto As String
+    Public Property direccion As String
+End Class
